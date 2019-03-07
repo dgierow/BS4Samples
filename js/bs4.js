e34bf41b7c1b5de0d2fd481662778547fa36cb3e
Douglas Gierow <gierow@gmail.com>
2019-03-07 12:36:58 -0700

    fixed margins page display issues. expanded lorem replacement code to use enum and give additional options.

--- js/bs4.js
+++ js/bs4.js
@@ -1,8 +1,32 @@
-function replaceWithLorem(selector, short = false) {
-    const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias eum possimus quibusdam quisquam. Cupiditate dolorem doloremque expedita harum itaque maxime modi numquam optio quae quidem repellendus totam, ullam unde.';
-    const loremShort = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, atque consectetur consequatur dolore';
+const loremLengths = {
+    TINY: 1,
+    SHORT: 2,
+    MEDIUM: 3,
+    LONG: 4
+};
 
-    const text = short ? loremShort : lorem;
+function replaceWithLorem(selector, length) {
+    const long = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem, culpa debitis dignissimos ducimus excepturi inventore ipsa nemo nostrum nulla officia omnis perferendis quas qui quis soluta, sunt, velit voluptates.';
+    const medium = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem, culpa debitis dignissimos ducimus excepturi inventore ipsa nemo nostrum nulla.';
+    const short = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem, culpa debitis.';
+    const tiny = 'Lorem ipsum dolor sit amet.';
+
+    let text = '';
+    switch(length){
+        case loremLengths.TINY:
+            text = tiny;
+            break;
+        case loremLengths.SHORT:
+            text = short;
+            break;
+        case loremLengths.MEDIUM:
+            text = medium;
+            break;
+        case loremLengths.LONG:
+            text = long;
+            break;
+    }
+
     const elements = document.querySelectorAll(selector);
     elements.forEach(x => x.innerText = text);
 }
