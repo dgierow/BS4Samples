78b981dddde379ac2b6fde97366eed59ec9e03cf
Douglas Gierow <gierow@gmail.com>
2019-03-07 09:38:55 -0700

    add collapsible menu for md screens and smaller. hide navbar on home page iframe thumbnails

--- js/bs4.js
+++ js/bs4.js
@@ -1,8 +1,8 @@
-function replaceWithLorem(className, short = false) {
+function replaceWithLorem(selector, short = false) {
     const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias eum possimus quibusdam quisquam. Cupiditate dolorem doloremque expedita harum itaque maxime modi numquam optio quae quidem repellendus totam, ullam unde.';
     const loremShort = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, atque consectetur consequatur dolore';
 
     const text = short ? loremShort : lorem;
-    const elements = Array.from(document.getElementsByClassName(className));
+    const elements = document.querySelectorAll(selector);
     elements.forEach(x => x.innerText = text);
 }
